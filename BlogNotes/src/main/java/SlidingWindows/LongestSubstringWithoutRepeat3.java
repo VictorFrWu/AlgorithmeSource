3f38f96eca1599908e3bbe30cca75f85c2a295bd
wuhe <dachichiwuhe@gmail.com>
2019-11-25 01:13:06 +0100

    first blog

--- BlogNotes/src/main/java/SlidingWindows/LongestSubstringWithoutRepeat3.java
+++ BlogNotes/src/main/java/SlidingWindows/LongestSubstringWithoutRepeat3.java
@@ -1,11 +1,11 @@
 package SlidingWindows;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
-
-
 
 /**
  * @author mac Given a string, find the length of the longest substring without
@@ -58,6 +58,27 @@ public class LongestSubstringWithoutRepeat3 {
 			res = Math.max(res, i - left + 1);
 		}
 		return res;
+	}
+
+	public int lengthOfLongestSubstring3(String s) {
+		int n = s.length();
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+			for (int j = i + 1; j <= n; j++)
+				if (allUnique(s, i, j))
+					ans = Math.max(ans, j - i);
+		return ans;
+	}
+
+	public boolean allUnique(String s, int start, int end) {
+		Set<Character> set = new HashSet<>();
+		for (int i = start; i < end; i++) {
+			Character ch = s.charAt(i);
+			if (set.contains(ch))
+				return false;
+			set.add(ch);
+		}
+		return true;
 	}
 
 	@Test
