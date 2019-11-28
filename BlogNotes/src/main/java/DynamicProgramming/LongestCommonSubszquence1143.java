4b6d4496f8357f9da7de97d28d26286fcb59d656
wuhe <dachichiwuhe@gmail.com>
2019-11-28 12:34:12 +0100

    largest common subsequence

---
+++ BlogNotes/src/main/java/DynamicProgramming/LongestCommonSubszquence1143.java
@@ -0,0 +1,98 @@
+package DynamicProgramming;
+
+import org.junit.Test;
+
+/**
+ * @author he.wu Given two strings text1 and text2, return the length of their
+ *         longest common subsequence.
+ * 
+ *         A subsequence of a string is a new string generated from the original
+ *         string with some characters(can be none) deleted without changing the
+ *         relative order of the remaining characters. (eg, "ace" is a
+ *         subsequence of "abcde" while "aec" is not). A common subsequence of
+ *         two strings is a subsequence that is common to both strings.
+ * 
+ * 
+ * 
+ *         If there is no common subsequence, return 0.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input: text1 = "abcde", text2 = "ace" Output: 3 Explanation: The
+ *         longest common subsequence is "ace" and its length is 3. Example 2:
+ * 
+ *         Input: text1 = "abc", text2 = "abc" Output: 3 Explanation: The
+ *         longest common subsequence is "abc" and its length is 3. Example 3:
+ * 
+ *         Input: text1 = "abc", text2 = "def" Output: 0 Explanation: There is
+ *         no such common subsequence, so the result is 0.
+ */
+public class LongestCommonSubszquence1143 {
+	public int longestCommonSubsequence(String text1, String text2) {
+		if (text1.length() == 0 || text2.length() == 0)
+			return 0;
+		int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+		for (int i = 0; i < text1.length(); i++) {
+			for (int j = 0; j < text2.length(); j++) {
+				if (text1.charAt(i) == text2.charAt(j)) {
+					dp[i + 1][j + 1] = dp[i][j] + 1;
+				} else {
+					dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+				}
+			}
+		}
+		return dp[text1.length()][text2.length()];
+	}
+
+	// print the longest common subsequence
+	public String longestCommonSubsequence2(String text1, String text2) {
+		if (text1.length() == 0 || text2.length() == 0)
+			return "";
+		int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+		for (int i = 0; i < text1.length(); i++) {
+			for (int j = 0; j < text2.length(); j++) {
+				if (text1.charAt(i) == text2.charAt(j)) {
+					dp[i + 1][j + 1] = dp[i][j] + 1;
+				} else {
+					dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+				}
+			}
+		}
+		int index = dp[text1.length()][text2.length()];
+		if (index == 0)
+			return "";
+		return printLCS(text1.length(), text2.length(), index, dp, text1, text2);
+	}
+
+	private String printLCS(int m, int n, int index, int[][] dp, String text1, String text2) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		int i = m, j = n;
+		while (i > 0 && j > 0) {
+			// If current character in X[] and Y are same, then
+			// current character is part of LCS
+			if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+				sb.append(text1.charAt(i - 1)); // Put current character in result
+				i--;
+				j--;
+				index--; // reduce values of i, j and index
+			}
+
+			// If not same, then find the larger of two and
+			// go in the direction of larger value
+			else if (dp[i - 1][j] > dp[i][j - 1])
+				i--;
+			else
+				j--;
+		}
+		return sb.reverse().toString();
+	}
+
+	@Test
+	public void test() {
+		String text1 = "abcde", text2 = "azcegx";
+		System.out.println(longestCommonSubsequence2(text1, text2));
+	}
+}
