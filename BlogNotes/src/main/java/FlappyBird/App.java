cea9a3eb16d70dc8e3a67a5efb7488186f0cc3e2
wuhe <dachichiwuhe@gmail.com>
2019-12-09 16:30:43 +0100

    falppy bird

---
+++ BlogNotes/src/main/java/FlappyBird/App.java
@@ -0,0 +1,24 @@
+package FlappyBird;
+
+import javax.swing.*;
+
+public class App {
+
+    public static int WIDTH = 500;
+    public static int HEIGHT = 520;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setTitle("Falppy Bird");
+        frame.setLocationRelativeTo(null);
+
+        Keyboard keyboard = Keyboard.getInstance();
+        frame.addKeyListener(keyboard);
+
+        GamePanel panel = new GamePanel();
+        frame.add(panel);
+    }
+}
