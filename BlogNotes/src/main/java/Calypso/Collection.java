403d45295f6ec4b6a1ad9967a99f79513d531d4f
wuhe <dachichiwuhe@gmail.com>
2020-02-12 21:51:37 +0100

    hashcode equald

--- BlogNotes/src/main/java/Calypso/Collection.java
+++ BlogNotes/src/main/java/Calypso/Collection.java
@@ -1,15 +1,28 @@
 package Calypso;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author mac
  *
- * interface 
- * iterable -> collection 
- *                |
- *    Class: abstractlist,abstractqueue,abstractset  Interface: list, set, queue
+ *         interface iterable -> collection | Class:
+ *         abstractlist,abstractqueue,abstractset Interface: list, set, queue
  *
  */
 
 public class Collection {
 
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		map.put(1, "A");
+		map.put(1, "B");
+		map.forEach((k, v) -> System.out.println("Item : " + k + " Count : " + v));
+
+		String a = new String("a");
+		String b = new String("a");
+		System.out.println(a.equals(b));
+		System.out.println(a == b);
+		System.out.println(a.hashCode() + " " + b.hashCode());
+	}
 }
