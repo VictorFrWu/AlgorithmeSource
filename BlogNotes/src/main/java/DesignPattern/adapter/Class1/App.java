3049e6335dad517630aa0ff73b7258e90c8f49a2
wuhe <dachichiwuhe@gmail.com>
2020-02-28 10:27:03 +0100

    s

--- BlogNotes/src/main/java/DesignPattern/adapter/Class1/App.java
+++ BlogNotes/src/main/java/DesignPattern/adapter/Class1/App.java
@@ -4,6 +4,12 @@ package DesignPattern.adapter.Class1;
  * @author he.wu
  *	1 adapter design pattern 
  *	high coupling but agile 
+ *
+ *
+ *spring handler 
+ *dispatch servelet 
+ *
+ *login - tomcat - dispatch servlet - handler mapping - model and view - execute method by adapter design pattern - applicationcontext xml configuration 
  */
 public class App {
 	public static void main(String[] args) {
